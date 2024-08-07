11faee8aaf140808cb27fe1f20b33d30be13c80b
Gabriel Moraes <gabrielmoraesweb@gmail.com>
2024-08-07 15:31:50 -0300

    Update logo

--- middleware.ts
+++ middleware.ts
@@ -1,16 +1,3 @@
-
-/*
-import { authMiddleware } from "@clerk/nextjs";
- 
-export default authMiddleware({
-  publicRoutes: ['/', '/api/webhooks/clerk', '/api/webhooks/stripe']
-});
- 
-export const config = {
-  matcher: ["/((?!.+\\.[\\w]+$|_next).*)", "/", "/(api|trpc)(.*)"],
-};
-*/
-
 import { authMiddleware } from '@clerk/nextjs/server';
 
 export default authMiddleware({
