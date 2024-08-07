52ecf4dc33b0a242d7ff2ff841eb1a298f94ab9e
Gabriel Moraes <gabrielmoraesweb@gmail.com>
2024-08-07 13:47:16 -0300

    Fix IImage interface

--- lib/database/models/image.model.ts
+++ lib/database/models/image.model.ts
@@ -1,14 +1,15 @@
 import { Document, Schema, model, models } from "mongoose";
 
 export interface IImage extends Document {
+  _id: Schema.Types.ObjectId; // Add this line
   title: string;
   transformationType: string;
   publicId: string;
-  secureURL: string; 
+  secureURL: string;
   width?: number;
   height?: number;
-  config?: object; 
-  transformationUrl?: string; 
+  config?: object;
+  transformationUrl?: string;
   aspectRatio?: string;
   color?: string;
   prompt?: string;
